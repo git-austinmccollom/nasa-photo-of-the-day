b48d3923a741fd15183a21729e0810d1679045d8
git-austinmccollom <austinmccollom@gmail.com>
2020-08-12 19:28:23 -0500

    finally have API content rendering on screen for the Post component. I have no idea what I changed to make it work, it doesn't look any different

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, {useState, useEffect} from "react";
-import Post from "./Components/Post"
+import Post from "./Components/Post";
 import "./App.css";
-import Axios from "axios";
+import axios from "axios";
+import date from "date-and-time";
 
 function App() {
-  const [post, setPost] = useState();
+  const [post, setPost] = useState({});
   const BASE_URL = 'https://api.nasa.gov/planetary/apod?api_key=1nZRJTT4osWq35OiAIXOHJsMaxERpf42eUdEu52d';
   // const HD_QUERY_PARAMETER = '&hd=True'; - May not need that, hdurl is always included in response.data
+  const TODAY = date.format(new Date(), 'YYYY-M-D');
+  console.log(TODAY);
 
   useEffect( () => {
-    Axios.get(`${BASE_URL}`)
+    axios.get(`${BASE_URL}&date=${TODAY}`)
     .then( response => {
-      console.log(response.data)
       setPost(response.data)
     })
     .catch( error => {
       debugger
     })
-  })
-
+  }, [])
+  console.log(post);
+  console.log(post.title);
   return (
     <div className="App">
       <Post post={post}/>
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function Post(props) {
-    const post = props
+    const {post} = props
+    console.log(post.title)
     return (
         <div className='post'>
-        <p>hello world</p>
-        <img src={post.hdurl}/>
+            <p>hello world?</p>
+            <p>{post.title}</p>
+            {/* <img src={`${hdurl}`}/> */}
         </div>
     )
 }
